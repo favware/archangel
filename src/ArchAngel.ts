46bcb4bfa6716b251c588a9590fb20c1114d83ad
Jeroen Claassens <support@favware.tech>
2021-02-26 00:20:52 +0100

    top level await goes brrrr

--- src/ArchAngel.ts
+++ src/ArchAngel.ts
@@ -4,14 +4,10 @@ import { TOKEN } from '#root/config';
 
 const client = new ArchAngelClient();
 
-async function main() {
-	try {
-		await client.login(TOKEN);
-	} catch (error) {
-		client.logger.error(error);
-		client.destroy();
-		process.exit(1);
-	}
+try {
+	await client.login(TOKEN);
+} catch (error) {
+	client.logger.error(error);
+	client.destroy();
+	process.exit(1);
 }
-
-void main();
