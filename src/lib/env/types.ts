d549d8bfdd106be372e3b423a49d7eb331371aa6
Jeroen Claassens <support@favware.tech>
2021-12-29 03:45:47 +0100

    chore: cleanup env vars

--- src/lib/env/types.ts
+++ src/lib/env/types.ts
@@ -10,19 +10,12 @@ export interface ArchAngelEnv {
 	NODE_ENV: 'test' | 'development' | 'production';
 	DOTENV_DEBUG_ENABLED: BooleanString;
 
-	CLIENT_NAME: string;
 	CLIENT_VERSION: string;
-	CLIENT_PREFIX: string;
-	CLIENT_REGEX_PREFIX: string;
-	CLIENT_OWNERS: string;
-	CLIENT_ID: string;
 
 	CLIENT_PRESENCE_NAME: string;
 	CLIENT_PRESENCE_TYPE: string;
 
 	COMMAND_GUILD_IDS: string;
-
-	WEBSOCKET_PORT: IntegerString;
 
 	DISCORD_TOKEN: string;
 }
